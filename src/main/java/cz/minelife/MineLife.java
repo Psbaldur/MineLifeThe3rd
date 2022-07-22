dd62b674fc308ffca8f2a6bba72a4efd4b1ca9a1
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-07-22 12:35:30 +0200

    Initial commit

---
+++ src/main/java/cz/minelife/MineLife.java
@@ -0,0 +1,17 @@
+package cz.minelife;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineLife extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
